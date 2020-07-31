e5021e59e9ee771fe51806c4f75e3dc901377014
ahinkle <ahinkle10@gmail.com>
2020-07-30 22:10:42 -0500

    refactor: styleguide basics controller

--- app/Http/Controllers/StyleguideBasicsController.php
+++ app/Http/Controllers/StyleguideBasicsController.php
@@ -45,14 +45,9 @@ class StyleguideBasicsController extends Controller
      */
     public function store(Request $request)
     {
-        $input = $request->input('basics');
-        $styleguideBasics = new StyleguideBasics();
+        $basics = collect($request->input('basics'))->mapWithKeys(fn($value) => [$value => 1]);
 
-        foreach ($input as $setting) {
-            $styleguideBasics->$setting = 1;
-        }
-
-        $styleguideBasics->save();
+        StyleguideBasics::create($basics->toArray());
 
         return redirect()->route('basics.index')->with('success', 'Styleguide color is successfully saved.');
     }
