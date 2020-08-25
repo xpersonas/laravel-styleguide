c053cf41fb0b791ba05ce2c4d4a8f32fe8a72d57
xpersonas <justin.neel@gmail.com>
2020-08-25 12:53:26 -0500

    phpcs cleanup redo

--- app/Providers/StyleguideNovaTool.php
+++ app/Providers/StyleguideNovaTool.php
@@ -8,7 +8,8 @@ use Xpersonas\Styleguide\Nova\StyleguideColor;
 use Xpersonas\Styleguide\Nova\StyleguideBasics;
 use Xpersonas\Styleguide\Nova\StyleguidePattern;
 
-class StyleguideNovaTool extends Tool {
+class StyleguideNovaTool extends Tool
+{
     /**
      * Perform any tasks that need to happen when the tool is booted.
      *
