8e72bc72e15ca8018fab6df907cf601a96450e98
xpersonas <justin.neel@gmail.com>
2020-10-13 15:19:33 -0500

    reorg, buttons in list - fixing publishing issues

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -15,6 +15,7 @@
     {{--Footer scripts & styles.--}}
     @section('footer')
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+        <link href="{{ asset('css/vendor/xpersonas/styleguide.css') }}" rel="stylesheet" type="text/css" />
 
         <style>
             body.simple-styleguide {
